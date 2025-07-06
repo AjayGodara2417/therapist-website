4090947bc045d2085a5ecb47c31a0a9965c65d11
Ajay Godara <agodara771@gmail.com>
2025-07-06 14:09:12 +0530

    ClientNotice Completed

---
+++ src/components/ClientNotice.tsx
@@ -0,0 +1,9 @@
+export default function ClientNotice() {
+  return (
+    <div className="min-h-[250px] bg-[#f7f4ed] flex items-center justify-center px-4">
+      <p className="text-center text-gray-800 font-serif text-lg sm:text-xl">
+        Unable to accept new clients at this time.
+      </p>
+    </div>
+  );
+}
