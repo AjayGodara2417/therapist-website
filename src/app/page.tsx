a6947fef57ef732c76b17f51ce0c5b5dedaf389f
Ajay Godara <agodara771@gmail.com>
2025-07-06 14:19:37 +0530

    Navbar & Footer fixed &  main page Completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
+import OceanBanner from "@/components/Banner";
+import About from "./about/page";
+import ServicesPage from "./services/page";
+import SessionFeeSection from "./sessionFee/page";
+import ClientNotice from "@/components/ClientNotice";
+import Hero from "./hero/page";
+import FAQSection from "./faq/page";
+import Contact from './contact/page'
 
 export default function Home() {
   return (
     <div>
-
+      <Hero />
+      <About />
+      <ServicesPage />
+      <SessionFeeSection />
+      <ClientNotice />
+      <OceanBanner />
+      <FAQSection />
+      <Contact />
     </div>
   );
 }
